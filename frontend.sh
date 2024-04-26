22bf501145be267e78a8c3a6e8ba5acc43fc52d5
alona19 <76895924+alona19@users.noreply.github.com>
2024-04-27 00:57:02 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -19,7 +19,5 @@ sudo apt-get install -y docker-ce
 # Add the ubuntu user to the docker group
 sudo usermod -aG docker ubuntu
 
-#Docker login
-sudo docker login -u alonageorge25219 -p Jeal@1908
 # Run a Docker container
 sudo docker run -d --name my-container -p 80:80 alonageorge25219/frontend:1.0.1
